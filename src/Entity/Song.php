0f88ad55467f7bab6653c589524a24465b2dbc11
JuanjeNieto <juanjenieto3107@gmail.com>
2024-02-22 21:05:19 +0100

    funcionar, funciona

---
+++ src/Entity/Song.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SongRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SongRepository::class)]
+class Song
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $artist = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $genre = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getArtist(): ?string
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(string $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    public function getGenre(): array
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(array $genre): static
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+}
